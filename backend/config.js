d294b0d2a5d77e6409daa0f39d50fa5a3eb48cba
Bryan Martinez <mbryan156@gmail.com>
2023-09-27 21:28:15 -0400

    MongoDb'd

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555; 
+export const mongoDBURL = 
+    'mongodb+srv://bryanmartinez:fuWAsGp3Ryg6xmkz@books-store-mern.lous7hm.mongodb.net/books-collection?retryWrites=true&w=majority';
+    
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express, { request, response } from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 
 const app = express();
@@ -12,6 +13,16 @@ app.get('/', (request, response) => {
 
 
 // check if server is running
-app.listen(PORT, () => {
-    console.log('App is listening to port: ' + PORT);
-});
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then( () => {
+        console.log('App connected to database'); 
+        app.listen(PORT, () => {
+            console.log('App is listening to port: ' + PORT);
+        });
+    })
+    .catch((error) => {
+        console.log(error); 
+    }); 
