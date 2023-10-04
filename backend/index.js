eb97215fe4a5353a4c1ffa36c81605307b8c105a
Bryan Martinez <mbryan156@gmail.com>
2023-10-03 22:08:12 -0400

    created get all books

--- backend/index.js
+++ backend/index.js
@@ -38,8 +38,26 @@ app.post("/books", async(request, response) =>{
     }
 }); 
 
+// get all books from database
+app.get("/books", async(request, response) =>{
+    try{
+        const books = await Book.find({}); 
+        return response.status(200).json({
+            count: books.length, 
+            data: books
+
+        }); 
+
+    }catch(error) {
+        console.log(error.message); 
+        response.status(500).send({ message: error.message}); 
+
+    }
+}); 
+
 
 // check if server is running
+
 
 
 mongoose
