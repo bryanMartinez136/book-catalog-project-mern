4c805dab9822d00628d8423945fda8f7cadd5aeb
Bryan Martinez <mbryan156@gmail.com>
2023-12-12 11:54:54 -0500

    Create page components and configure endpoints

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ShowBook from './pages/ShowBook';
+import DeleteBook from './pages/DeleteBook';
+import EditBook from './pages/EditBook';
+import CreateBooks from './pages/CreateBooks';
 
 export const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+
+      {/* routes for 5 different pages.  */}
+      <Route path='/' element={<Home/>} />
+      <Route path='/books/create' element={<CreateBooks/>} />
+      <Route path='/books/details/:id' element={<ShowBook/>} />
+      <Route path='/books/edit/:id' element={<EditBook/>} />
+      <Route path='/books/delete/:id' element={<DeleteBook/>} />
+    </Routes>
   )
 }
 
