9ea49d9af6634d916ab89aa17883e68f0ecf8c0a
Bryan Martinez <mbryan156@gmail.com>
2023-12-12 18:26:11 -0500

    Updated mongoDb

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,2 @@
 export const PORT = 5555; 
-export const mongoDBURL = 
-    'mongodb+srv://bryanmartinez:fuWAsGp3Ryg6xmkz@books-store-mern.lous7hm.mongodb.net/books-collection?retryWrites=true&w=majority';
-    
+export const mongoDBURL = process.env.MONGODB_URL
