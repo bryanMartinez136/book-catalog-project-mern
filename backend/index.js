60e04af8d1d62ddeafd0a3d455a67697d8989cbe
Bryan Martinez <mbryan156@gmail.com>
2023-12-12 10:44:59 -0500

    Finished creating the server, added cors policy

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,26 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors'
 
 
 const app = express();
 
 //Middleware for parsing request body
 app.use(express.json()); 
+
+//middle ware for handling cors policy 
+app.use(cors())
+
+//allow custom origins, you have more contorl
+//only uses with these origins can access the server
+app.use(
+    cors({
+        origin: 'http://localhost:3000', 
+        methods: ['GET','POST', 'PUT', 'DELETE'], 
+        allowedHeaders: ['Content-Type'], 
+    })
+)
 
 // create a new route
 app.get('/', (request, response) => {
